98bcf792d2e1323a498f8763af05fd04efbaa78d
k-iwagami <51388959+k-iwagami@users.noreply.github.com>
2021-01-06 13:37:16 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+      {{ msg }}
+    </p>
+    <p v-else>no text</p>
+    <input type="text" v-model="msg" />
+    <button @click="clear()">clear</button>
+    <button @click="created()">created</button>
+    <br />
+    <button @click="microcms()">microcms</button>
+    <template v-if="contents != null">
+      <h1>{{ contents.title }}</h1>
+      <h2>日付: {{ contents.publishedAt }}</h2>
+      <p v-html="contents.body"></p>
+    </template>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import myheader from "./components/myheader";
+import axios from "axios";
+import config from "./config/development";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld
-  }
+    myheader,
+  },
+  data() {
+    return {
+      msg: "Hello World!",
+      contents: null,
+    };
+  },
+  methods: {
+    clear() {
+      this.msg = "";
+    },
+
+    created() {
+      console.log("created");
+      fetch(
+        "https://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US"
+      )
+        .then((response) => {
+          console.log(response.status);
+          return response.json();
+        })
+        .then((json) => {
+          this.msg = json.postalcodes[0].adminName1;
+        })
+        .catch((err) => {
+          this.msg = err; // エラー処理
+        });
+    },
+
+    async microcms() {
+      try {
+        const res = await axios.get(
+          "https://iwagami.microcms.io/api/v1/news/xvjaw8sfg",
+          {
+            headers: {
+              "X-API-KEY": config.api_key,
+            },
+          }
+        );
+        this.contents = res.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
